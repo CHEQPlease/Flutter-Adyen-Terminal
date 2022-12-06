f9e01a4f20be4fe383b0dd73d987b16bedcdeda2
Niaz Ahmed <itsniaz@gmail.com>
2022-12-06 15:45:01 +0600

    WIP: Model Sync Flutter to Kotlin

--- lib/data/AdyenTerminalConfig.dart
+++ lib/data/AdyenTerminalConfig.dart
@@ -1,57 +1,55 @@
-import 'dart:convert';
-
-AdyenTerminalConfig adyenTerminalConfigFromJson(String str) => AdyenTerminalConfig.fromJson(json.decode(str));
-String adyenTerminalConfigToJson(AdyenTerminalConfig data) => json.encode(data.toJson());
 class AdyenTerminalConfig {
   AdyenTerminalConfig({
-      required this.endpoint,
-      required this.merchantId,
-      required this.environment,
-      required this.keyId,
-      required this.keyPassphrase,
-      required this.merchantName,
-      required this.keyVersion,});
+      this.endpoint, 
+      this.merchantId, 
+      this.terminalModelNo, 
+      this.terminalSerialNo, 
+      this.terminalId, 
+      this.environment, 
+      this.keyId, 
+      this.keyPassphrase, 
+      this.merchantName, 
+      this.keyVersion, 
+      this.certPath,});
 
   AdyenTerminalConfig.fromJson(dynamic json) {
     endpoint = json['endpoint'];
     merchantId = json['merchantId'];
+    terminalModelNo = json['terminalModelNo'];
+    terminalSerialNo = json['terminalSerialNo'];
+    terminalId = json['terminalId'];
     environment = json['environment'];
     keyId = json['key_id'];
     keyPassphrase = json['key_passphrase'];
     merchantName = json['merchant_name'];
     keyVersion = json['key_version'];
+    certPath = json['certPath'];
   }
-  String? endpoint;
-  String? merchantId;
-  String? environment;
-  String? keyId;
-  String? keyPassphrase;
-  String? merchantName;
-  String? keyVersion;
-AdyenTerminalConfig copyWith({  String? endpoint,
-  String? merchantId,
-  String? environment,
-  String? keyId,
-  String? keyPassphrase,
-  String? merchantName,
-  String? keyVersion,
-}) => AdyenTerminalConfig(  endpoint: endpoint ?? this.endpoint,
-  merchantId: merchantId ?? this.merchantId,
-  environment: environment ?? this.environment,
-  keyId: keyId ?? this.keyId,
-  keyPassphrase: keyPassphrase ?? this.keyPassphrase,
-  merchantName: merchantName ?? this.merchantName,
-  keyVersion: keyVersion ?? this.keyVersion,
-);
+  String endpoint;
+  String merchantId;
+  String terminalModelNo;
+  String terminalSerialNo;
+  String terminalId;
+  String environment;
+  String keyId;
+  String keyPassphrase;
+  String merchantName;
+  String keyVersion;
+  String certPath;
+
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['endpoint'] = endpoint;
     map['merchantId'] = merchantId;
+    map['terminalModelNo'] = terminalModelNo;
+    map['terminalSerialNo'] = terminalSerialNo;
+    map['terminalId'] = terminalId;
     map['environment'] = environment;
     map['key_id'] = keyId;
     map['key_passphrase'] = keyPassphrase;
     map['merchant_name'] = merchantName;
     map['key_version'] = keyVersion;
+    map['certPath'] = certPath;
     return map;
   }
 
