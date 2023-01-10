e3405edf0e4bf700812254e400cca2b6a29501dc
Niaz Ahmed <itsniaz@gmail.com>
2023-01-10 18:26:54 +0600

    Loseless Image Compression R&D to keep image within 100KB

---
+++ android/src/main/kotlin/com/itsniaz/adyen/adyen_terminal_payment/Dimension.java
@@ -0,0 +1,11 @@
+public class Dimension {
+
+    public int width;
+    public   int height;
+
+    public Dimension(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+}
