ae2dbb9fe3cf4e8429ff7d5a6342cf92efdec5cd
Niaz Ahmed <itsniaz@gmail.com>
2023-01-13 15:31:10 +0600

    ADDED a set of layout for small screens

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -111,98 +111,100 @@ class _MyAppState extends State<MyApp> {
 
 
                             FlutterAdyen.init(terminalConfig);
+
+                            /* Demo Payload for testing*/
                             String receiptDTOJSON =
                             """
                            
-{
-  "brandName": "CHEQ Diner1",
-  "orderType": "Self-Order",
-  "orderSubtitle": "Kiosk-Order",
-  "totalItems": "2",
-  "orderNo": "K10",
-  "tableNo": "234",
-  "receiptType": "kiosk",
-  "timeOfOrder": "Placed at : 01/12/2023 03:57 AM AKST",
-  "items": [
-    {
-      "itemName": "Salmon Fry",
-      "description": "  -- Olive\n  -- Deep Fried Salmon\n  -- ADD Addition 1\n  -- no Nuts\n  -- no Olive Oil\n  -- Substitution 1 SUB\n  -- allergy 1 ALLERGY\n",
-      "quantity": "1",
-      "price": "\$10.0",
-      "strikethrough": false
-    },
-    {
-      "itemName": "Water + Apple Pay",
-      "description": "  -- Onions\n",
-      "quantity": "1",
-      "price": "\$1.0",
-      "strikethrough": true
-    }
-  ],
-  "breakdown": [
-    {
-      "key": "Payment Type",
-      "value": "Card",
-      "important": null
-    },
-    {
-      "key": "Card Type",
-      "value": "mc",
-      "important": null
-    },
-    {
-      "key": "Card #:",
-      "value": "541333 **** 9999",
-      "important": null
-    },
-    {
-      "key": "Card Entry",
-      "value": "CONTACTLESS",
-      "important": null
-    },
-    {
-      "key": "",
-      "value": "",
-      "important": null
-    },
-    {
-      "key": "Sub Total",
-      "value": "\$21.01",
-      "important": null
-    },
-    {
-      "key": "Area Tax",
-      "value": "\$1.00",
-      "important": null
-    },
-    {
-      "key": "VAT",
-      "value": "\$2.10",
-      "important": null
-    },
-    {
-      "key": "Customer Fee",
-      "value": "\$0.63",
-      "important": null
-    },
-    {
-      "key": "Service Fee",
-      "value": "\$0.91",
-      "important": null
-    },
-    {
-      "key": "Tax",
-      "value": "\$0.01",
-      "important": null
-    },
-    {
-      "key": "GRAND TOTAL",
-      "value": "\$25.66",
-      "important": true
-    }
-  ]
-}
-                            
+                              {
+                                "brandName": "CHEQ Diner1",
+                                "orderType": "Self-Order",
+                                "orderSubtitle": "Kiosk-Order",
+                                "totalItems": "2",
+                                "orderNo": "K10",
+                                "tableNo": "234",
+                                "receiptType": "kiosk",
+                                "timeOfOrder": "Placed at : 01/12/2023 03:57 AM AKST",
+                                "items": [
+                                  {
+                                    "itemName": "Salmon Fry",
+                                    "description": "  -- Olive\n  -- Deep Fried Salmon\n  -- ADD Addition 1\n  -- no Nuts\n  -- no Olive Oil\n  -- Substitution 1 SUB\n  -- allergy 1 ALLERGY\n",
+                                    "quantity": "1",
+                                    "price": "\$10.0",
+                                    "strikethrough": false
+                                  },
+                                  {
+                                    "itemName": "Water + Apple Pay",
+                                    "description": "  -- Onions\n",
+                                    "quantity": "1",
+                                    "price": "\$1.0",
+                                    "strikethrough": true
+                                  }
+                                ],
+                                "breakdown": [
+                                  {
+                                    "key": "Payment Type",
+                                    "value": "Card",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Card Type",
+                                    "value": "mc",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Card #:",
+                                    "value": "541333 **** 9999",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Card Entry",
+                                    "value": "CONTACTLESS",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "",
+                                    "value": "",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Sub Total",
+                                    "value": "\$21.01",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Area Tax",
+                                    "value": "\$1.00",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "VAT",
+                                    "value": "\$2.10",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Customer Fee",
+                                    "value": "\$0.63",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Service Fee",
+                                    "value": "\$0.91",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "Tax",
+                                    "value": "\$0.01",
+                                    "important": null
+                                  },
+                                  {
+                                    "key": "GRAND TOTAL",
+                                    "value": "\$25.66",
+                                    "important": true
+                                  }
+                                ]
+                              }
+                                                          
                             """
                             ;
                             FlutterAdyen.printReceipt(_get10DigitNumber(),receiptDTOJSON, onSuccess: (String val){
